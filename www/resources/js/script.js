aa4aaf59a470a52eccd6b12a98816fafa76274ee
aazambrano2 <aazambrano2@miners.utep.edu>
2020-11-04 16:58:41 -0700

    Added javascript files and a css file to observe how html is embedded in java script and using html rules to link a css file for styling. Practice variables on JavaScript.

--- www/resources/js/script.js
+++ www/resources/js/script.js
@@ -1,2 +1,7 @@
 document.write("Script.js");
 alert("the file is working");
+//Javascript code where using HTML but linking with a CSS file
+document.write("<link rel='stylesheet' href='C:/Users/Crene/PHP_Practice/php_practice/www/resources/css/styles.css'></link>")
+document.write("<h2 id = 'jmain'>Javascript Rules!<h2>");
+document.write("<hr>")
+
---
+++ www/resources/js/variables.js
@@ -0,0 +1,6 @@
+var phrase = "To be or not to be";
+document.write(phrase);
+document.write(phrase);
+document.write(phrase);
+document.write(phrase);
+document.write(phrase);

59b1284f9394f1712677be717063b52ec2977bb7
aazambrano2 <aazambrano2@miners.utep.edu>
2020-11-05 09:32:43 -0700

    Practicing more variables and strings on Java script. Enabled Compiled Hero and created a string.dev.js file

---
+++ www/resources/js/strings.js
@@ -0,0 +1,19 @@
+var phrase = "Strings are coool "
+document.write("Strings are fun",'<br>'); //"" or ''
+document.write(phrase,'<br>');
+document.write(phrase.length,'<br>');
+document.write(phrase.toUpperCase(),'<br>');
+document.write(phrase.toLowerCase(),'<br>');
+document.write(phrase.charAt(3),'<br>');
+//if there are duplicates, the first occuernce index will be returned. Else -1 if no char exist.
+document.write(phrase.indexOf("i"),'<br>'); 
+//get the index of the last duplicate 
+document.write(phrase.lastIndexOf("o"),'<br>'); 
+
+document.write(phrase.substring(0,3),'<br>');
+document.write(phrase.substring(phrase.lastIndexOf("o"),phrase.length),'<br>');//ol
+document.write(phrase.endsWith("coool"),'<br>');
+var text = "Strings";
+document.write(phrase.includes(text),'<br>');
+document.write(phrase + "YES! " + text,'<br>');
+//Source: https://www.w3schools.com/jsref/jsref_obj_string.asp
--- www/resources/js/variables.js
+++ www/resources/js/variables.js
@@ -1,6 +1,13 @@
-var phrase = "To be or not to be";
+//Variables are dynamic (no type)
+var phrase = "Aaron";
+var age = 23
+var gpa = 3.69
+var isMale = true;
+var flaws = null;
+var description = undefined; //not null just has not been defined.
+document.write(phrase," ",age," ",gpa);
 document.write(phrase);
 document.write(phrase);
-document.write(phrase);
+phrase = "YES!"
 document.write(phrase);
 document.write(phrase);

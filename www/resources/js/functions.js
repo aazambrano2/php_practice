372916413b2ffc727b0c8ae695ad2e43d0635f5b
aazambrano2 <aazambrano2@miners.utep.edu>
2020-11-06 12:06:15 -0700

    Practiced functinos on JavaScript

---
+++ www/resources/js/functions.js
@@ -0,0 +1,11 @@
+function sayHi(name,age){
+    document.write("<h1>Hello "+ name +"</h1>");
+    document.write("<p>You are "+ age +"</p>");
+    alert("Hey");
+}
+function add(n1,n2){
+    return n1 + n2;
+}
+sayHi("Aaron",22);
+
+document.write(add(1,2));

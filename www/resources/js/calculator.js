67c19147efbf162c07733f28fafb84640033b451
aazambrano2 <aazambrano2@miners.utep.edu>
2020-11-06 11:18:01 -0700

    Implemented a simple calculator from practicing prompt in JavaScript

---
+++ www/resources/js/calculator.js
@@ -0,0 +1,7 @@
+//return as strings
+var num1 = window.prompt("Enter a number");
+var num2 = window.prompt("Enter another number");
+//parse strings to int
+num3 = parseInt(num1) + parseInt(num2);
+//parseFloat()
+document.write(num3);

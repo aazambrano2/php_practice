ea8ed725ae81dd53f144afb72093730b41bd879d
aazambrano2 <aazambrano2@miners.utep.edu>
2020-11-06 09:16:42 -0700

    Practicing mathmatical methods in Java Script.

---
+++ www/resources/js/numbers.js
@@ -0,0 +1,20 @@
+document.write(2,'<br>');
+document.write(2.434,'<br>');
+document.write(2+2,'<br>');
+document.write(2-2,'<br>');
+document.write(2/2,'<br>');
+document.write(2*2,'<br>');
+document.write(2*(2+7),'<br>');
+var num = 6;
+document.write(num*(2+7),'<br>');
+document.write(3%2,'<br>');
+
+document.write("Math Methods",'<br> <hr>');
+var num2 = -2;
+document.write(Math.abs(num2),'<br>');
+document.write(Math.max(num2,4),'<br>');
+document.write(Math.min(num2,4),'<br>');
+document.write(Math.round(4.6),'<br>');
+document.write(Math.pow(num,2),'<br>');
+document.write(Math.sqrt(36),'<br>');
+document.write(Math.round(Math.random()*10),'<br>');

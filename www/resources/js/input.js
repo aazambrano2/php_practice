cf209e8d55c98ad05c266f0c9a2a952bc58b3d60
aazambrano2 <aazambrano2@miners.utep.edu>
2020-11-06 09:36:09 -0700

    Added input.js and calculator.js . Practiced how to promt user for input in JavaScript.

---
+++ www/resources/js/input.js
@@ -0,0 +1,3 @@
+var name = window.prompt("What is your name?");
+var age = window.prompt("How old are you?");
+document.write("Hey " + name + ". How's it going? Do you like being " + age + " years old?");

d7bfdf78900a55c6240593f5ecc9c01d3bdd1618
aazambrano2 <aazambrano2@miners.utep.edu>
2020-11-09 16:45:04 -0700

    Practice event listners within the htmlAcess.j file and observed mouseout and mouse over listners.

--- www/resources/js/dist/htmlAccess.dev.js
+++ www/resources/js/dist/htmlAccess.dev.js
@@ -11,14 +11,33 @@ link.href = "https://my.utep.edu/My";
 link.style = "color : blue ;"; //css string
 //Event Listeners
 
-function handleClick() {
-  alert("Hi.....hehehehehe");
+function handleClick(element) {
+  //HTML element
+  element.innerHTML = "Clicked";
+  element.style = "background-color:blue;";
 }
 
 function video() {
   //alert("test");
-  var video = "https://www.youtube.com/watch?v=xUEqDQOjAlA&ab_channel=ALDBARANofficiel"; //https://www.utep.edu/ 
+  var video = "https://www.youtube.com/watch?v=YnplRWla99Q&ab_channel=UTEPMiners"; //https://www.utep.edu/ 
   //https://www.youtube.com/watch?v=xUEqDQOjAlA&ab_channel=ALDBARANofficiel
 
   return video;
 }
+
+var image = document.getElementById("image"); //alternative on click event listener
+//mouseover -> when the mouse cursor is hovered 
+
+/*function -> when the user interacts with an image/button/etc 
+and what is executed when that event happens.*/
+
+image.addEventListener("mouseover", function () {
+  this.style = "box-shadow: 2px 2px 2px grey";
+  this.width = "110";
+}); //mouseout -> when the mouse cursor out from the image
+//onmouseout vs mouseout -> on if you are using in the html file; not needed in addEventListener() method
+
+image.addEventListener("mouseout", function () {
+  this.style = "";
+  this.width = "100";
+});
--- www/resources/js/htmlAccess.js
+++ www/resources/js/htmlAccess.js
@@ -13,17 +13,37 @@ link.style = "color : blue ;" //css string
 //Event Listeners
 
 
-function handleClick(){
-    alert("Hi.....hehehehehe");
-    
+function handleClick(element){
+    //HTML element
+    element.innerHTML = "Clicked";
+    element.style = "background-color:blue;"
 }
 
 function video(){
     
     //alert("test");
-    var video = "https://www.youtube.com/watch?v=xUEqDQOjAlA&ab_channel=ALDBARANofficiel";
+    var video = "https://www.youtube.com/watch?v=YnplRWla99Q&ab_channel=UTEPMiners";
     //https://www.utep.edu/ 
     //https://www.youtube.com/watch?v=xUEqDQOjAlA&ab_channel=ALDBARANofficiel
-    return video
-    
+    return video 
 }
+
+var image = document.getElementById("image");
+//alternative on click event listener
+//mouseover -> when the mouse cursor is hovered 
+/*function -> when the user interacts with an image/button/etc 
+and what is executed when that event happens.*/
+image.addEventListener("mouseover",function(){
+    this.style ="box-shadow: 2px 2px 2px grey";
+    this.width = "110"
+
+});
+
+//mouseout -> when the mouse cursor out from the image
+
+//onmouseout vs mouseout -> on if you are using in the html file; not needed in addEventListener() method
+image.addEventListener("mouseout",function(){
+    this.style ="";
+    this.width = "100"
+    
+});

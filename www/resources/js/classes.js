8253fef09f9f9298c6ce0c42be5c3fd6e1f5aae0
aazambrano2 <aazambrano2@miners.utep.edu>
2020-11-11 11:45:41 -0700

    Practice Classes in Java and noted the difference bewteen private and public attributes in Java Script (note not actually using the keywords since it is not available but the concepts behind how to implement them.

--- www/resources/js/classes.js
+++ www/resources/js/classes.js
@@ -1,3 +1,38 @@
+
+//Source: https://www.youtube.com/watch?v=2ZphE5HcQPQ&list=WL&index=1&t=1168s&ab_channel=freeCodeCamp.org
+class Rectangle{
+    //how to set up private attributes
+    #width;
+    #height;
+    #color;
+    //set up your object
+    // public and private attributes can only be used in TypeScript
+    /*Compile hero will complain since it does not identify the # */
+    constructor(_width,_height,_color){
+    
+        console.log("The Rectangle is being created!");
+
+        this.#width = _width;
+        this.#height = _height;
+        this.#color = _color;
+        /*
+        public attributes - note the missing #
+        this.width = _width;
+        this.height = _height;
+        this.color = _color;
+        */
+    }
+
+     getArea(){
+        return this.#width * this.#height;
+    }
+
+    printDescription(){
+        console.log('I am a rectangle of ' + this.#width + 'x' + this.#height + ' and I am ' + this.#color);
+    }
+
+}
+
 //objects
 var person = {
     name: "Aaron",
@@ -9,6 +44,40 @@ var person = {
     }
 };
 
-person.printName();
+var myMovie = {
+    title: "The Social Network",
+    releaseYear: "2010",
+    duration: 2.0,
+    actors: [
+        {
+            name: "Jessie Eisenberg",
+            birthday: new Date("October 5, 1983"),
+            hometown: "New York, New York"
+        },
+        {
+            name: "Rooney Mara",
+            birthday: new Date("April 17, 1985"),
+            hometown: "Bedford, New York"
+        }
+    ],
+    printMovieInfo: function(){
+        document.write(this.title + "<br>")
+        document.write("Released: " + this.releaseYear + "<br>")
+        document.write("Runtime: " + this.duration+ " hrs <br>")
+        document.write("Starring: " + this.actors[0].name + " and " + this.actors[1].name + "<br>")
+    }
+};
 
-//TODO: Look at classes
+
+
+//TODO: Look at rest of source video for classes
+//var - globally - can be viewed through the program let- block scope
+var myRectangle1 = new Rectangle(10, 15, "red");
+var myRectangle2 = new Rectangle(20,45,"blue");
+console.log(myRectangle1.getArea());
+console.log(myRectangle2.getArea());
+myRectangle1.printDescription();
+myRectangle2.printDescription();
+person.printName();
+myMovie.printMovieInfo();
+
--- www/resources/js/dist/classes.dev.js
+++ www/resources/js/dist/classes.dev.js
@@ -1,5 +1,7 @@
 "use strict";
 
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
 //objects
 var person = {
   name: "Aaron",
@@ -10,4 +12,18 @@ var person = {
     document.write("<h1>" + this.name + "</h1>");
   }
 };
-person.printName(); //TODO: Look at classes
+person.printName(); //Source: https://www.youtube.com/watch?v=2ZphE5HcQPQ&list=WL&index=1&t=1168s&ab_channel=freeCodeCamp.org
+
+var Rectangle = //set up your object
+function Rectangle(_width, _height, _color) {
+  _classCallCheck(this, Rectangle);
+
+  console.log("The Rectangle is being created!");
+  this.width = _width;
+  this.height = _height;
+  this.color = _color;
+}; //var - globally - can be viewed through the program let- block scope
+
+
+var myRectangle1 = new Rectangle(10, 15, "red");
+var myRectangle2 = new Rectangle(20, 45, "blue");
